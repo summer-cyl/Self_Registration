23fab7cb58b17b0c919ce214a8fb5e2465baa164
shadowlay <1042944044@qq.com>
2018-05-09 21:17:08 +0800

    index

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var aa=new zturn({
+	id:"zturn",
+	opacity:0.9,
+	width:382,
+	Awidth:1024,
+	scale:0.9,
+	auto:true,//是否轮播 默认5000
+	turning:1000//轮播时长
+})
+var ab=new zturn({
+	id:"zturn2",
+	opacity:0.8,
+	width:382,
+	Awidth:1024,
+	scale:0.6,
+	auto:true
+})
