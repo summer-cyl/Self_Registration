b17f73a45d51b28d4658e7e68e992d5aaa5805a7
fansofzyx <274561843@qq.com>
2018-05-13 12:52:04 +0800

    加了

--- Self_Registration For Patient/src/wlzj/js/index.js
+++ Self_Registration For Patient/src/wlzj/js/index.js
@@ -1,13 +1,49 @@
+var name = "null"
 $('.hnunit').click(function() {
 	$(this).addClass('hnuclick').siblings().removeClass('hnuclick');
 	var address = $(this).attr('address');
 	$('#contentFrame').attr('src', address);
 });
 
-$('#caselogin').click(function() {
+$('#caselogin1').click(function() {
     $('#loginDiv').attr('style', 'display: block');
 });
 
 $('.closeihconpop').click(function() {
     $('#loginDiv').attr('style', 'display: none');
+});
+$('#login').click(function(){
+	if ($("#userName").val() == "") {
+        alert("用户名不能为空！");
+        $("#userName").focus();
+        return false;
+    }
+    if ($("#password").val() == "") {
+        alert("密码不能为空！");
+        $("#password").focus();
+        return false;
+    }
+    $.ajax({
+        type: "POST",
+        url: "/LoginDeal",
+        data: "name=" + $("#userName").val().toString() + "&password=" + $("#password").val().toString(),
+        success: function (data) {
+        	alert("成功！");
+            if (data == "1") {
+                $("#caselogin1").attr('style','display: none');
+                $("#caselogin2").attr('style','display: block');
+                $('#usernamespan').html($("#userName").val().toString());
+                $('#loginDiv').attr('style', 'display: none');
+                return true;
+            }
+            else {
+                alert("请确认您输入的用户名或密码输入是否正确！");
+                $("#userName").val("");
+                $("#password").val("");
+                $("#userName").focus();
+                return false;
+            }
+        }
+
+    })
 });
